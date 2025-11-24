81115f194f42292f6d9e3d130552acf982a33f27
Sperhak <73948698+Sperhak323@users.noreply.github.com>
2025-11-24 12:51:04 +0100

    Update Settings.tsx

--- plugins/role-icon-changer/src/Settings.tsx
+++ plugins/role-icon-changer/src/Settings.tsx
@@ -1,22 +1,50 @@
 import { storage } from "@vendetta/plugin";
 import { Forms } from "@vendetta/ui/components";
-import React from "react";
+import React, { useState } from "react";
+// FormRow a FormInput sú pre vkladanie dát
+const { FormSection, FormRow, FormInput } = Forms; 
 
-// Použijeme FormInput namiesto FormText, aby sa dalo písať
-// Ak by FormInput nefungoval, vrátime sa k FormText na test
-const { FormSection, FormInput } = Forms;
+// Inicializácia pre nové polia, ak neexistujú
+if (!storage.targetId) storage.targetId = "";
+if (!storage.targetUrl) storage.targetUrl = "";
 
-export default () => (
-    <FormSection title="Nastavenia Ikon">
-        <FormInput
-            value={storage.iconMapJson || ""}
-            placeholder='{"ID": "URL"}'
-            onChange={(v) => {
-                // Jednoduché uloženie priamo do pamäte pri každom údere klávesy
-                storage.iconMapJson = v;
-            }}
-            style={{ height: 100 }}
-            multiline={true}
-        />
-    </FormSection>
-)
+export default () => {
+    // Používame useState na sledovanie zmien
+    const [idInput, setIdInput] = useState(storage.targetId);
+    const [urlInput, setUrlInput] = useState(storage.targetUrl);
+
+    return (
+        <FormSection title="Nastavenie ikony pre jedného užívateľa">
+            
+            {/* Pole pre ID */}
+            <FormRow 
+                label="ID Používateľa"
+                sublabel="Vložte presné 18-miestne ID."
+            >
+                <FormInput
+                    placeholder="1106246400158728242"
+                    value={idInput}
+                    onChange={(newValue) => {
+                        setIdInput(newValue);
+                        storage.targetId = newValue; // Uloženie ID
+                    }}
+                />
+            </FormRow>
+            
+            {/* Pole pre URL */}
+            <FormRow 
+                label="URL Adresa Ikony"
+                sublabel="Odkaz (URL) na obrázok (napr. .png)."
+            >
+                <FormInput
+                    placeholder="https://i.postimg.cc/..."
+                    value={urlInput}
+                    onChange={(newValue) => {
+                        setUrlInput(newValue);
+                        storage.targetUrl = newValue; // Uloženie URL
+                    }}
+                />
+            </FormRow>
+        </FormSection>
+    );
+};
